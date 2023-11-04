883b3e7c8d60f89cab70448165d3398761ba3615
Korakoe <56580073+korakoe@users.noreply.github.com>
2023-11-04 19:26:56 +0800

    Test Zero Shot Voice Cloning

--- library_test.py
+++ library_test.py
@@ -1,11 +1,14 @@
 from vallex.utils.generation import SAMPLE_RATE, generate_audio, preload_models
+from vallex.utils.prompt_making import make_prompt
 from scipy.io.wavfile import write as write_wav
 
 # download and load all models
 model, codec, vocos = preload_models(download=True) # checkpoint_path=Whatever you want (CUSTOM MODEL GALORE)
 
+make_prompt(name="paimon", audio_prompt_path="paimon_prompt.wav",
+            transcript="Just, what was that? Paimon thought we were gonna get eaten.")
 
 while True:
-    audio_array = generate_audio(model, codec, vocos, input("what to say: "))
+    audio_array = generate_audio(model, codec, vocos, input("what to say: "), prompt="paimon")
     write_wav("test1.wav", SAMPLE_RATE, audio_array)
 
